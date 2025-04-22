9dedb704b0fa1ce05902194f638acb3cef2fc65a
Md Nuruzzaman <nur.mnzaman@gmail.com>
2025-04-23 00:36:36 +0600

    feat: reports and transaction page create

---
+++ app/(main)/reports/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Reports from "../_components/Reports";
+
+const page = () => {
+  return (
+    <>
+      <Reports />
+    </>
+  );
+};
+
+export default page;
---
+++ app/(main)/transactions/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Transactions from '../_components/Transactions'
+
+const page = () => {
+  return (
+    <div><Transactions/></div>
+  )
+}
+
+export default page

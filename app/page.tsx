cc4c338fee4f282c0a832aa8500e767d39a00331
Md Nuruzzaman <nur.mnzaman@gmail.com>
2025-04-24 23:46:56 +0600

    feat : fetch api in root app folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import { baseUrl } from "@/lib/utils";
 import Dashboard from "./(main)/dashboard/_components/Dashboard";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch(`${baseUrl}/api/transactions`, {
+    cache: 'no-store'
+  });
+  
+  if (!res.ok) {
+    throw new Error('Failed to fetch transactions');
+  }
+  
+  const transactions = await res.json(); // Added await here
+
+  console.log("transactions", transactions);
   return (
     <>
-      <Dashboard />
+      <Dashboard transactions={transactions} /> 
     </>
   );
 }

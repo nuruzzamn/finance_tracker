f0acc3f200e6942f1df1d65e8fffcd4db693160d
Md Nuruzzaman <nur.mnzaman@gmail.com>
2025-04-25 12:25:29 +0600

    feat (Reports): fetch transaction data

--- app/(main)/reports/_components/Reports.tsx
+++ app/(main)/reports/_components/Reports.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { CategoryChart } from "../../_components/CategoryChart";
 import { IncomeExpenseChart } from "../../_components/IncomeExpenseChart";
+import { baseUrl } from "@/lib/utils";
 
-const Reports = () => {
+const Reports = async () => {
+  const res = await fetch(`${baseUrl}/api/transactions`, {
+      cache: 'no-store'
+    });
+    
+    if (!res.ok) {
+      throw new Error('Failed to fetch transactions');
+    }
+    
+    const transactions = await res.json();
+    
   return (
     <div className="min-h-screen py-8 px-4 md:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto space-y-8">
@@ -16,10 +27,10 @@ const Reports = () => {
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
           <div className="bg-white dark:bg-gray-800 rounded-xl shadow-sm p-6 border border-gray-200 dark:border-gray-700">
-            <CategoryChart />
+            <CategoryChart transactions={transactions.data}/>
           </div>
           <div className="bg-white dark:bg-gray-800 rounded-xl shadow-sm p-6 border border-gray-200 dark:border-gray-700">
-            <IncomeExpenseChart />
+            <IncomeExpenseChart transactions={transactions.data}/>
           </div>
         </div>
       </div>
